f1ab37178f68e9e7b516e871dda66b598c3ff6b2
Elsayed H Elsory <elsayed.7amada@gmail.com>
2021-07-04 18:09:08 +0200

    Load The Ui File And create the mainLoop class✔

--- app.py
+++ app.py
@@ -7,3 +7,23 @@ import sys
 import os
 import urllib.request
 
+# load the UI file
+FORM_CLASS, _ = loadUiType(os.path.join(os.path.dirname(__file__), "DesignDownloader.ui"))
+
+# create the main loop
+class MainLoop(QMainWindow, FORM_CLASS):
+    def __init__(self, parent=None):
+        super(MainLoop).__init(parent)
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainLoop()
+    window.show()
+    app.exec_()
+
+
+if __name__ == "__main__":
+    main()
