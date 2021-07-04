b39a09579f5a8ca8882131c4764ed0ca44620f06
Elsayed H Elsory <elsayed.7amada@gmail.com>
2021-07-04 18:00:40 +0200

    Import The Needed Modules To Start The App👍

---
+++ app.py
@@ -0,0 +1,9 @@
+# Import needed modules 
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.uic import *
+import sys
+import os
+import urllib.request
+
